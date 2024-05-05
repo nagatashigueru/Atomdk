203f1af637cae1371ef2264a413f75c41aee9fa2
shigueru nagata <shiguerunagata@protonmail.com>
2024-05-04 23:55:35 -0400

    Se creo el modulo input.py

---
+++ input.py
@@ -0,0 +1,7 @@
+# --- #
+# Nombre      : input.py
+# Descripción : Modulo que implementa el metodo para leer las opciones desde un archivo de
+#               entrada (input).
+# Fecha       : 04-05-2014
+# Autor       : Shigueru Nagata
+# --- #
